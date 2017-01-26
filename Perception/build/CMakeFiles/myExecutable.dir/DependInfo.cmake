749c16c3567e787e3f52a627865e67c893ba8ad4
Vasudha Todi <todivasudha@gmail.com>
2017-01-26 09:05:20 +0530

    Added cmake file

---
+++ Perception/build/CMakeFiles/myExecutable.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vasudha/AGV_api/Perception/src/LidarData.cpp" "/home/vasudha/AGV_api/Perception/build/CMakeFiles/myExecutable.dir/src/LidarData.cpp.o"
+  "/home/vasudha/AGV_api/Perception/src/MonoCamData.cpp" "/home/vasudha/AGV_api/Perception/build/CMakeFiles/myExecutable.dir/src/MonoCamData.cpp.o"
+  "/home/vasudha/AGV_api/Perception/src/StereoCamData.cpp" "/home/vasudha/AGV_api/Perception/build/CMakeFiles/myExecutable.dir/src/StereoCamData.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  "qh_QHpointer"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/opencv"
+  "/usr/include/vtk-5.8"
+  "/usr/include/pcl-1.7"
+  "/usr/include/eigen3"
+  "/usr/include/ni"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ Perception/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vasudha/AGV_api/Perception
+# Build directory: /home/vasudha/AGV_api/Perception/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
