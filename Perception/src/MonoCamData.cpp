7c6d0b91720dac03e30c9e3deec0a4e551bdc374
Vasudha Todi <todivasudha@gmail.com>
2017-01-26 07:04:30 +0530

    Added class for Monocular Camera data

--- Perception/src/MonoCamData.cpp
+++ Perception/src/MonoCamData.cpp
@@ -1,1 +1,18 @@
 #include "include/MonoCamData"
+
+MonoCamData::MonoCamData(){}
+
+MonoCamData::MonoCamData(cv::Mat new_image)
+{
+	image = new_image;
+}
+
+MonoCamData::getImage()
+{
+	return image;
+}
+
+MonoCamData::setImage(cv::Mat img)
+{
+	image = img;
+}
