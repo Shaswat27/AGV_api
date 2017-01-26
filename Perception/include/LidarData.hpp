84c82430128c83b32396376da7da88d56171065f
Vasudha Todi <todivasudha@gmail.com>
2017-01-26 08:07:00 +0530

    Added data type for Lidar

---
+++ Perception/include/LidarData.hpp
@@ -0,0 +1,30 @@
+/*
+	Data type for 3D Lidar
+	Author : Vasudha Todi
+*/
+
+#ifndef LIDARDATA_HPP
+#define LIDARDATA_HPP
+
+#include <pcl/point_types.hpp>
+#include <pcl/PCLPointCloud2.h>
+
+class LidarData{
+
+	//point cloud 
+	pcl::PointCloud<pcl::PointXYZI>::Ptr cloud;
+
+	public :
+		//Constructors
+		LidarData();
+		LidarData(pcl::PointCloud<pcl::PointXYZI>::Ptr new_cloud);
+
+		//get cloud data
+		pcl::PointCloud<pcl::PointXYZI>::Ptr getCloud();
+
+		//set cloud data
+		void setCloud(pcl::PointCloud<pcl::PointXYZI>::Ptr cl);
+
+};
+
+#endif
