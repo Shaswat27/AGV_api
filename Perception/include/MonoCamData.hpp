4bf41ba3927b935cc01b25524f035856778cfec2
Vasudha Todi <todivasudha@gmail.com>
2017-01-26 07:34:58 +0530

    Added documentation

--- Perception/include/MonoCamData.hpp
+++ Perception/include/MonoCamData.hpp
@@ -1,3 +1,7 @@
+/*
+	Data type for monocular camera
+	Author : Vasudha Todi
+*/
 
 #ifndef MONOCAMDATA_HPP
 #define MONOCAMDATA_HPP
@@ -8,9 +12,14 @@ class MonoCamData{
 	cv::Mat image;
 
 	public:
+		//Constructors
 		MonoCamData();
 		MonoCamData(cv::Mat new_image);
+
+		//get image data
 		cv:Mat getImage();
+
+		//set image
 		cv:Mat setImage(cv::Mat img);
 };
 
