993b939fcbdb152580c72d8272e93f8b23621e6c
Vasudha Todi <todivasudha@gmail.com>
2017-01-29 21:36:30 +0530

    Created namespace for preprocessing algorithms

---
+++ include/Perception/Preprocessing.hpp
@@ -0,0 +1,12 @@
+/*
+	Preprocessing algorithms
+*/
+
+#ifndef PREPROCESSING_HPP
+#define PREPROCESSING_HPP
+
+//Namespace for the algortihms, all classes and functions to be declared within it
+namespace Preprocessing
+{
+
+}
